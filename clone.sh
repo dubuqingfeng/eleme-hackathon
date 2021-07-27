a3993fd7c5ac2a8961cca1702714c3e5cc986e79
dubuqingfeng <1135326346@qq.com>
2021-07-27 17:20:31 +0800

    add clone.sh

---
+++ clone.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/WhiteBlue/eleme_hackathon.git WhiteBlue
+git clone https://github.com/nicoding/Hackathon.git nicoding
+git clone https://github.com/ele828/eleme-hackathon.git ele828
+# git clone https://github.com/tcOops/hackathon-eleme.git tcOops
+git clone https://github.com/garzon/eleme_hackathon.git garzon
+git clone https://github.com/ManfredHu/hackathon-java.git ManfredHu
+git clone https://github.com/chenqingzhu/BuyingWebServer.git chenqingzhu
+git clone https://github.com/zyearn/eleme-hackathon.git  zyearn
+git clone https://github.com/liumx10/hackathon-go.git liumx10
+git clone https://github.com/Declan94/eleme-hackathon.git Declan94
+# git clone https://github.com/denglixi/hackathon.git denglixi
+git clone https://github.com/fcjy/eleme-hackathon.git fcjy
+git clone https://github.com/langruixiang/eleme_hackathon.git langruixiang
